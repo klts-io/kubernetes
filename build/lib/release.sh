87b88f5e77d2cdab9815578ff3bb24e8635eda54
Shiming Zhang <wzshiming@foxmail.com>
2021-07-22 18:41:45 +0800

    No delete images

--- build/lib/release.sh
+++ build/lib/release.sh
@@ -364,8 +364,8 @@ function kube::release::create_docker_images_for_server() {
         rm -rf "${docker_build_path}"
         ln "${binary_file_path}.tar" "${images_dir}/"
 
-        kube::log::status "Deleting docker image ${docker_image_tag}"
-        "${DOCKER[@]}" rmi "${docker_image_tag}" &>/dev/null || true
+        # kube::log::status "Deleting docker image ${docker_image_tag}"
+        # "${DOCKER[@]}" rmi "${docker_image_tag}" &>/dev/null || true
       ) &
     done
 
